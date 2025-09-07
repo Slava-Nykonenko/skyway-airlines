81fdfe36eb90951cd7209cf6edd825a576ecefd1
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-09-07 13:22:21 +0100

    configured airline/urls.py, added management/urls.py

--- management/urls.py
+++ management/urls.py
@@ -14,18 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import (
-    path,
-    include
-)
 
-from airline import settings
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("management.urls", namespace="management")),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("__debug__/", include("debug_toolbar.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+app_name = "management"
