8a5f1e8c4e4210b4fd3978e6152f3bd8078d04c0
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-09-18 20:11:07 +0100

    staticfiles managing

--- airline/settings/base.py
+++ airline/settings/base.py
@@ -16,7 +16,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -41,6 +40,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -74,7 +74,6 @@ WSGI_APPLICATION = "airline.wsgi.application"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-
 AUTH_USER_MODEL = "management.Staff"
 
 # Password validation
@@ -107,7 +106,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 DATE_FORMAT = "d/m/Y"
 
 DATETIME_FORMAT = "d/m/Y H:i"
@@ -119,7 +117,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- airline/settings/prod.py
+++ airline/settings/prod.py
@@ -4,7 +4,8 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+# ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
