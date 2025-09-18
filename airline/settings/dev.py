a42da4716cf30171d104ae1c78772f58ddb90c07
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-09-18 13:44:45 +0100

    fixed warnings raised by check deploy

--- airline/settings/dev.py
+++ airline/settings/dev.py
@@ -17,3 +17,5 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- airline/settings/prod.py
+++ airline/settings/prod.py
@@ -19,3 +19,7 @@ DATABASES = {
         "PORT": int(os.environ["POSTGRES_DB_PORT"]),
     }
 }
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
