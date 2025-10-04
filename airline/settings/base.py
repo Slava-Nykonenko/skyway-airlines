fb1a8b4da01bba0cf96244a405287f7319dbaa7c
Viacheslav Nykonenko <nykonenkoviacheslav@gmail.com>
2025-10-04 15:13:13 +0100

    Update base.py

--- airline/settings/base.py
+++ airline/settings/base.py
@@ -113,9 +113,9 @@ DATETIME_FORMAT = "d/m/Y H:i"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "staticfiles/"
+STATIC_URL = "static/"
 
-STATICFILES_DIRS = (BASE_DIR / "staticfiles",)
+STATICFILES_DIRS = (BASE_DIR / "static",)
 
 STATIC_ROOT = "staticfiles"
 
